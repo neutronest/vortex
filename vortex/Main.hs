1b48d4182f0bc87bd44fba4d05af34402a8c3e80
neutronest <neutronest@gmail.com>
2016-03-05 04:53:15 +0900

    main text case dev

--- vortex/Main.hs
+++ vortex/Main.hs
@@ -7,4 +7,11 @@ import Numeric.LinearAlgebra
 main :: IO()
 main =
   do
-    
+    let inputLayer = inputLayerInit 2 2
+    let sigmoidLayer = sigmoidLayerInit 2 2
+    let outputLayer = linearLayerInit 2 2 in
+      let model = Model [inputLayer, sigmoidLayer, outputLayer] in
+      let input = (1><2)[1.0, 2.0]::Matrix R in
+      let output = forward input model in
+      print output
+      
