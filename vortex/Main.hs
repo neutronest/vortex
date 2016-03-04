edbd88acb64b5c8b462bf17f7956dd6a4bb44e76
neutronest <neutronest@gmail.com>
2016-03-05 02:23:50 +0900

    add main file

--- vortex/Main.hs
+++ vortex/Main.hs
@@ -3,11 +3,8 @@ import Layer
 import Models
 import Numeric.LinearAlgebra
 
+
 main :: IO()
 main =
   do
-    let mA = (2><2) [1.0, 2.0 , 3.0, 4.0] :: Matrix R
-    let mB = (2><2) [2.0, 2.0, 2.0, 2.0] :: Matrix R
-    let mC = mA + mB
-    let mAString = show mC in
-      putStrLn mAString
+    
