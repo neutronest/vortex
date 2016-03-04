97a0b70bdd6a804f64ac94b374147796cd273554
neutronest <neutronest@gmail.com>
2016-03-05 02:23:41 +0900

    model dev

--- vortex/Models.hs
+++ vortex/Models.hs
@@ -2,9 +2,22 @@ module Models where
 
 import Layer
 import Numeric.LinearAlgebra
+import Data.List
+
 
 data Model a = Model [a]
 
-addLayer :: CommonLayer a -> Model a -> Model a
-addLayer layer model = model:layer
-  
+addLayer :: Model a -> a -> Model a
+addLayer (Model []) layer = Model [layer]
+addLayer (Model l) layer = Model (l++[layer])
+
+forwardInterface :: Matrix R -> VLayer -> Matrix R
+forwardInterface input layer =
+  let (VLayer {layerType, rowNum, colNum, weight, bias, val}) = layer in
+  case layerType of
+    InputLayer -> forwardInputLayer input layer
+    SIgmoidLayer -> forwardSigmoidLayer input layer
+
+forward :: Matrix R -> Model a -> Matrix R
+forward input (Model []) = input
+forward input (Model xs) = foldl forwardInterface input xs
