8af4331ff4e5c6808879cd177663cc6721fb69c0
neutronest <neutronest@gmail.com>
2016-03-27 18:46:01 +0900

    add Loss

--- vortex/Loss.hs
+++ vortex/Loss.hs
@@ -3,6 +3,12 @@ module Loss where
 import Numeric.LinearAlgebra
 
 
+getLoss :: String -> Matrix R -> Matrix R -> Matrix R
+getLoss lossType yMat tMat =
+  case lossType of
+    "mse" -> mse yMat tMat
+    otherwise -> yMat -- Warning: Never Used Necessery!
+
 mse :: Matrix R -> Matrix R -> Matrix R
 mse yMat tMat =
   let dyMat = yMat - tMat
